5175c362c29f56fc2362c24abecc0ee2b36df694
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2018-09-11 17:50:29 +0530

    added sound

--- src/Components/AudioManager.jsx
+++ src/Components/AudioManager.jsx
@@ -2,38 +2,39 @@ import React from "react";
 const { Provider, Consumer } = React.createContext();
 
 export class AudioProvider extends React.Component {
-  constructor(props) {
-    super(props);
-    this.methods = { playPick: this.playPick, playDrop: this.playDrop };
-  }
-  dropEl = React.createRef();
-  pickEl = React.createRef();
-  playPick = async () => {
-    try {
-      await this.pickEl.current.play();
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  playDrop = async () => {
-    try {
-      await this.dropEl.current.play();
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  render() {
-    return (
-      <div>
-        <audio ref={this.dropEl}>
-          <source src="/assets/audio/drop.mp3" type="audio/mp3" />
-        </audio>
-        <audio ref={this.pickEl}>
-          <source src="/assets/audio/pick.mp3" type="audio/mp3" />
-        </audio>
-        <Provider value={this.methods}>{this.props.children}</Provider>
-      </div>
-    );
-  }
-}
+         constructor(props) {
+           super(props);
+           this.methods = { playPick: this.playPick, playDrop: this.playDrop, silent: this.silent };
+         }
+         dropEl = React.createRef();
+         pickEl = React.createRef();
+         silent=(value=true)=>{
+             this.pickEl.current.muted = value;
+             this.dropEl.current.muted = value;
+         }
+         playPick = async (silent) => {
+             try {
+               await this.pickEl.current.play();
+             } catch (e) {
+               console.log(e);
+             }
+         };
+         playDrop = async silent => {
+             try {
+               await this.dropEl.current.play();
+             } catch (e) {
+               console.log(e);
+             }
+         };
+         render() {
+           return <div>
+               <audio muted ref={this.pickEl} src="/assets/audio/pick.mp3" type="audio/mp3" />
+               <audio muted ref={this.dropEl} src="/assets/audio/drop.mp3" type="audio/mp3" />
+
+               <Provider value={this.methods}>
+                 {this.props.children}
+               </Provider>
+             </div>;
+         }
+       }
 export const AudioConsumer = Consumer;
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -6,6 +6,7 @@ import "../styles/home.css";
 import UpdateBanner from "../Components/UpdateBanner";
 import * as ServiceWorker from "../service-worker";
 
+import { AudioConsumer } from "../Components/AudioManager";
 class Home extends React.Component {
   state = {
     showBanner: false
@@ -15,9 +16,14 @@ class Home extends React.Component {
       this.setState({ showBanner: true });
     });
   }
+  clickHandler = silent => {
+    silent(false);
+    this.props.history.replace("/new-game");
+  };
   render() {
     const { props } = this;
-    return <div className="home">
+    return (
+      <div className="home">
         {this.state.showBanner && <UpdateBanner />}
         <div className="logo shape1">
           <div className="shape2" />
@@ -25,11 +31,16 @@ class Home extends React.Component {
           <div className="shape2" />
           <div className="shape2 shape3" />
         </div>
-        <Button onClick={e => props.history.replace("/new-game")}>Play</Button>
+        <AudioConsumer>
+          {({ silent }) => (
+            <Button onClick={e => this.clickHandler(silent)}>Play</Button>
+          )}
+        </AudioConsumer>
         <Link onClick={e => props.history.replace("/history")}>
           View History
         </Link>
-      </div>;
+      </div>
+    );
   }
 }
 export default Home;
