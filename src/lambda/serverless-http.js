5f60792de75fd62124782e1136dbcf8ba8b01d94
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2018-09-16 14:05:31 +0530

    [fix]:path to express app

--- src/lambda/serverless-http.js
+++ src/lambda/serverless-http.js
@@ -1,6 +1,6 @@
 /* example using https://github.com/dougmoscrop/serverless-http */
 import serverless from "serverless-http";
-import expressApp from "./app";
+import expressApp from "./App";
 
 // We need to define our function name for express routes to set the correct base path
 const functionName = "serverless-http";
