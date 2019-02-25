86285ab4e9fee9a844d4e0714ec8ad443702057a
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-02-25 17:13:24 +0530

    added og tags

--- api/share.js
+++ api/share.js
@@ -9,17 +9,36 @@ module.exports = function(req, res) {
   const html = `
     <html>
         <head>
-        <title>stackstickers</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+            <meta name="theme-color" content="#000000">
+
+            <link rel="icon" type="image/png" href="/assets/icons/icon.png"/>
+            <link rel="shortcut icon" href="/assets/icons/icon.png">
+
+            <title>Play Photo Pieces</title>
+            <meta name="description" content="Play Photo Pieces. Photo Pieces is a fun and engaging free online game. Play it!">
+            <meta property="og:title" content="Play Photo Pieces">
+            <meta property="og:description" content="Play Photo Pieces. Photo Pieces is a fun and engaging free online game. Play it!">
+            <meta property="og:site_name" content="Photo Pieces">
+            <meta property="og:type" content="website">
+            <meta property="fb:app_id" content="858847830905833" />
+            <meta property="og:image" content="/assets/images/social-media.png">
+            <meta property="og:url" content="https://photo-pieces.now.sh/">
+            
+            <meta name="twitter:title" content="Play Photo Pieces">
+            <meta name="twitter:url" content="https://photo-pieces.now.sh/">
             <meta name="twitter:card" content="summary_large_image">
             <meta name="twitter:site" content="@kuldeepkeshwar">
             <meta name="twitter:creator" content="@kuldeepkeshwar">
             <meta name="twitter:title" content="Play Photo Pieces">
-            <meta name="twitter:description" content="played Photo Pieces scored ${total} level ${levels}">
+            <meta name="twitter:description" content="I scored ${total} level ${levels}. Play Photo Pieces. Photo Pieces is a fun and engaging free online game. Play it!">
             <meta name="twitter:image" content="https://photo-pieces.now.sh/api/screenshot?stats=${stats}">
             <style>
                 body {
                     background: url(https://photo-pieces.now.sh/api/screenshot?stats=${stats});
                     background-repeat: no-repeat;
+                    background-color: #20273e;
                 }
             </style>
         </head>
