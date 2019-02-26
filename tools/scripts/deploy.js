10e8a99028a003df972065a8d65d2fd9ac2ce16c
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-02-26 12:36:58 +0530

    added TRACKING_ID

--- tools/scripts/deploy.js
+++ tools/scripts/deploy.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
+require('dotenv').config()
+
 const execa = require('execa');
 const { alias } = require('./../../now.json');
+
+const envs = `TRACKING_ID=${ process.env.TRACKING_ID}`;
+console.log(envs);
 (async () => {
     try{
-        const {stdout} = await execa('now',  ['--public']);
+        const {stdout} = await execa('now',  ['-e',envs,'--public']);
         const alisaExec= execa('now',  ['alias', stdout , `${alias[0]}.now.sh`]);
         alisaExec.stdout.pipe(process.stdout);
         alisaExec.stderr.pipe(process.stderr);
