d96051fd8bae77b3c410a60649e786e86d992654
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-05-13 11:04:22 +0530

    added robot.txt

--- src/Components/withTracker.js
+++ src/Components/withTracker.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactGA from "./../utils/ga";
 
+import PackageJson from "./../../package.json";
 const withTracker = WrappedComponent => {
   const trackPage = page => {
     ReactGA.set({ page });
+    ReactGA.set({CustomAppVersion:PackageJson.version})
     ReactGA.pageview(page);
   };
 
